d55f2af86cc102af9025d412900fb60db3d852f8
acrosson <alexcrosson@gmail.com>
2017-11-03 17:13:03 -0700

    Fixed bugs

--- layers/spat_soft_arg_max.py
+++ layers/spat_soft_arg_max.py
@@ -1,8 +1,13 @@
 
+import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
+from torch.autograd import Variable
 
 class SpatSoftArgMax(nn.Module):
+
     """Spatial Soft ArgMax Layer for pytorch as used in 
     https://arxiv.org/pdf/1504.00702.pdf aka Spatial Softmax
 
@@ -10,15 +15,15 @@ class SpatSoftArgMax(nn.Module):
     https://github.com/tensorflow/tensorflow/issues/6271#issuecomment-266893850
 
     """
-    def __init__(self, h, w):
+
+    def __init__(self):
         """
         inputs:
             h   (int)   height of input
             w   (int)   width of input
         """ 
         super(SpatSoftArgMax, self).__init__()
-        self.sm2d = nn.Softmax2d()
-        self.image_coords = nn.Parameter(torch.zeros((1, h, w, 2)))
+        self.sm = MySoftmax()
         
     def forward(self, x):
         """
@@ -27,10 +32,38 @@ class SpatSoftArgMax(nn.Module):
         returns:
             x   (torch.Tensor)  output tensor of shape [N, C, 2]
         """
-        x = self.sm2d(x)
-        x = x.unsqueeze(-1)
-        x = self.image_coords * x
-        x = x.sum(2).sum(2)
-        return x
+        batch_size, num_fp, num_rows, num_cols = x.size()
+        num_rows, num_cols, num_fp = [int(x) for x in [num_rows, num_cols, num_fp]]
+        x_map = np.empty([num_rows, num_cols], np.float32)
+        y_map = np.empty([num_rows, num_cols], np.float32)
 
+        for i in range(num_rows):
+            for j in range(num_cols):
+                x_map[i, j] = (i - num_rows / 2.0) / num_rows
+                y_map[i, j] = (j - num_cols / 2.0) / num_cols
+
+                # alternative scale map
+                #x_map[i, j] = j
+                #y_map[i, j] = i
+
+        x_map = Variable(torch.from_numpy(x_map)).cuda()
+        y_map = Variable(torch.from_numpy(y_map)).cuda()
+
+        x_map = x_map.view(-1, num_rows * num_cols)
+        y_map = y_map.view(-1, num_rows * num_cols)
+
+        features_ = x.view(batch_size, num_fp, num_rows * num_cols)
+        softmax = self.sm(features_)
+
+        fp_x = (x_map * softmax).sum(2)
+        fp_y = (y_map * softmax).sum(2)
+
+        fp = torch.stack((fp_x, fp_y), 2)
+        return fp
+
+class MySoftmax(nn.Module):
+    def forward(self, input_):
+        batch_size = input_.size()[0]
+        output_ = torch.stack([F.softmax(input_[i]) for i in range(batch_size)], 0)
+        return output_
 
